3eb28936ddd82443290cd1c2eebacceaf8aab8e3
pankajflows <36482818+pankajflows@users.noreply.github.com>
2026-06-01 12:27:50 +0530

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,47 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        def check(row, col, board):
+            # print("check", row, col, board)
+            for i in range(row): # any prev queens in same col ?
+                if board[i][col] == 'Q':
+                    return False
+
+            i = row-1
+            j = col-1
+            p = row-1
+            q = col+1
+            while i>=0 and j>=0:
+                if board[i][j] == 'Q':
+                    return False
+                i -= 1
+                j -= 1
+            while p>=0 and q<n:
+                if board[p][q] == 'Q':
+                    return False
+                p -= 1
+                q += 1
+            # print("True")
+            return True
+
+
+        board = [['.' for i in range(n)] for j in range(n)]
+
+        def magic(row, board):
+            if row == n:
+                # print(board)
+                ans.append([''.join(row) for row in board])
+                return
+
+            for i in range(n):
+                # print(row, i)
+                board[row][i] = 'Q'
+                if check(row, i, board): # checks if the current places queen is safe from previous queens
+                    magic(row+1, board)
+                board[row][i] = '.'
+
+
+        ans = list()
+        magic(0, board)
+        return ans
+        
+        
